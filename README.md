06e070939d39ccf398541588d8d36ccdfc30c8d3
João Yuri Anderson <66644628+UppityFurball13@users.noreply.github.com>
2025-12-30 01:46:54 +0000

    modificacao de frase e outros textos

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # cursoDev-clone-tabnews
 Projeto para treinar programação a partir do cursoDev fazendo um clone do tabnews
+
--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,29 @@
 function Home() {
-    return <h1>Para minha querida do cabelo em chamas, eu lhe amo!</h1>
+    return <div> 
+        <h1>Para minha querida do cabelo em chamas</h1>;
+        <p>Quero que saiba que em algum futuro, espero ser<br></br> 
+        a pessoa que você sonha que eu seja, <br></br>
+        espera e o marido que você sempre quis. <br></br>
+        Não sou perfeito, ninguém é, porém, saiba que estarei<br></br>
+        tentando ao máximo ser seu fiel companheiro. <br></br>
+        Da mesma forma que espero que você seja a minha melhor amiga,<br></br>
+        companheira, a pessoa em quem confio, que possamos ser eternos namorados.
+        <br></br>
+        <p>Não é o melhor site que já fiz, mas quero que esse seja<br></br>
+            o que utilizarei como energia para melhorar, <br></br>
+            para ser melhor não só a você, mas para o meu pessoal.<br></br> 
+            Tem certas realizações que só podemos ter a partir do <br></br>
+            nosso ponto de vista, um alivio, um "ainda bem", mesmo que você consiga entender, <br></br>
+            não será/terá a mesma sensação que quem está realizando teve.<br></br>
+            Tem um pouco de tom de despedida ou algo melancolico em certo nivel, <br></br>
+            mas só queria poder dizer o quanto lhe amo, lhe deixar registrado esse amor, <br></br>
+            e deixar marcado que esse é um dos vários projetos que possam a vir e moldar um futuro programador.
+            </p>
+        <br></br>
+        <br></br>
+        <strong>Te amo</strong>
+        </p>
+    </div>
 }
 
 export default Home;
